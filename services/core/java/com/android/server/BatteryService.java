ffc8db0320d663393206a01ab26562b947b73254
Ido Ben-Hur <idoybh2@gmail.com>
2017-09-17 19:28:50 +0800

    base: Let maintainers mention they don't have a notification led
    this saves devices w/o it some observers and runtime
    
    Change-Id: I0e05074c2e2ecc28f9d7aa8b3f4baa1d4fe82a03
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -200,6 +200,7 @@ public final class BatteryService extends SystemService {
     private Led mLed;
 
     //Battery light color customization
+    private boolean mHasLed;
     private boolean mLightEnabled = false;
     private boolean mFullBatteryLight = true;
     private boolean mAllowBatteryLightOnDnd;
@@ -239,6 +240,9 @@ public final class BatteryService extends SystemService {
                 com.android.internal.R.bool.config_hasVoocCharger);
         mHasTurboPower = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_hasTurboPowerCharger);
+
+        mHasLed = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_hasNotificationLed);
 
         mCriticalBatteryLevel = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel);
@@ -302,9 +306,9 @@ public final class BatteryService extends SystemService {
                         false, obs, UserHandle.USER_ALL);
                 updateBatteryWarningLevelLocked();
             }
-        } else if (phase == PHASE_BOOT_COMPLETED) {
-            SettingsObserver mObserver = new SettingsObserver(new Handler());
-            mObserver.observe();
+        } else if (mHasLed && phase == PHASE_BOOT_COMPLETED) {
+            SettingsObserver observer = new SettingsObserver(new Handler());
+            observer.observe();
         }
     }
 
@@ -1401,6 +1405,10 @@ public final class BatteryService extends SystemService {
          * Synchronize on BatteryService.
          */
         public void updateLightsLocked() {
+            // don't do anything if we don't have a led
+            if (!mHasLed) {
+                return;
+            }
             if (mBatteryLight == null) {
                 return;
             }
