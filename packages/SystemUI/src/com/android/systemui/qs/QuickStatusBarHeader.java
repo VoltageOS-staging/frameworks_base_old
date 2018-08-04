a212352271e0009d5dec6fba806053b193aa19bf
Pranav Vashi <neobuddy89@gmail.com>
2018-08-04 23:07:56 +0530

    Use position tunable to hide clock as well
    * There is no need for separate status bar tuner switch for this.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -67,6 +67,11 @@ import java.util.List;
 public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tunable,
         View.OnClickListener, View.OnLongClickListener {
 
+    private static final int CLOCK_POSITION_LEFT = 2;
+    private static final int CLOCK_POSITION_HIDE = 3;
+
+    private static final String STATUS_BAR_CLOCK =
+            "system:" + Settings.System.STATUS_BAR_CLOCK;
     public static final String STATUS_BAR_BATTERY_STYLE =
             "system:" + Settings.System.STATUS_BAR_BATTERY_STYLE;
     public static final String QS_BATTERY_STYLE =
@@ -200,6 +205,7 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
         updateResources();
 
         Dependency.get(TunerService.class).addTunable(this,
+                STATUS_BAR_CLOCK,
                 SHOW_QS_CLOCK,
                 SHOW_QS_DATE,
                 STATUS_BAR_BATTERY_STYLE,
@@ -657,6 +663,11 @@ public class QuickStatusBarHeader extends FrameLayout implements TunerService.Tu
     @Override
     public void onTuningChanged(String key, String newValue) {
         switch (key) {
+            case STATUS_BAR_CLOCK:
+                int showClock =
+                        TunerService.parseInteger(newValue, CLOCK_POSITION_LEFT);
+                mClockView.setClockVisibleByUser(showClock != CLOCK_POSITION_HIDE);
+                break;
             case SHOW_QS_CLOCK:
                 mShowClock =
                         TunerService.parseIntegerSwitch(newValue, true);
