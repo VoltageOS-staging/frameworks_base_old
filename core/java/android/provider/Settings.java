39665e74b1c208270f86ccda462c726a9f983c7f
maxwen <max.weninger@gmail.com>
2019-09-25 16:19:49 +0200

    base: navigation mode settings [1/2]
    to configure the height of the back gesture
    
    Change-Id: I6ebe0039e250f3b8f0b91b232df5941b58026499
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6160,6 +6160,9 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
 
+	/** @hide */
+        public static final String BACK_GESTURE_HEIGHT = "back_gesture_height";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
