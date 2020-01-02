57d6d3a49b503b5bd43b03eee0fda9f8521f8265
Anushek Prasal <anushekprasal@gmail.com>
2020-01-03 00:18:59 +0530

    base: Add Touch HIDL support
    All credits to LineageOS and related authors on:
    https://github.com/LineageOS/android_lineage-sdk
    
    Ported to AOSP from lineage-sdk by SKULSHADY
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Iac8dba8b2d509fe1fa707efc4da9c661f1321d9d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5752,6 +5752,12 @@ public final class Settings {
         public static final String LOCKSCREEN_ALBUMART_FILTER = "lockscreen_albumart_filter";
 
         /**
+         * Whether or not to vibrate when a touchscreen gesture is detected
+         * @hide
+         */
+        public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -5936,6 +5942,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
             PRIVATE_SETTINGS.add(POCKET_JUDGE);
             PRIVATE_SETTINGS.add(VOLUME_KEY_CURSOR_CONTROL);
+            PRIVATE_SETTINGS.add(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK);
         }
 
         /**
