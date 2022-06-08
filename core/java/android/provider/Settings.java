36dc409988802ba07417aba74b38902391f7f88a
timjosten <timjosten@gmail.com>
2022-06-08 20:13:53 +0900

    base: Improve IME space feature [1/2]
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6153,10 +6153,12 @@ public final class Settings {
         public static final String GAMESPACE_SUPPRESS_FULLSCREEN_INTENT = "gamespace_suppress_fullscreen_intent";
 
 	/**
-         * boolean value. toggles narrow navigation bar in gestural nav mode
+         * Gesture navbar IME space.
+         * Supported modes: 0 for normal length, 1 for narrow and 2 for hidden.
+         * Default 0.
          * @hide
          */
-        public static final String NAVIGATION_BAR_GESTURAL_NARROW = "navigation_bar_gestural_narrow";
+        public static final String NAVIGATION_BAR_IME_SPACE = "navigation_bar_ime_space";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
