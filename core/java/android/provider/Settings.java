d9afe86a5574d3fc53c738e8a5ca7b25888402e9
Dmitrii <bankersenator@gmail.com>
2022-08-29 11:13:50 +0000

    SystemUI: old style mobile data indicators [1/2]
    neobuddy89: Use Tuner API.
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5581,6 +5581,11 @@ public final class Settings {
         public static final String LESS_BORING_HEADS_UP = "less_boring_heads_up";
 
         /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
