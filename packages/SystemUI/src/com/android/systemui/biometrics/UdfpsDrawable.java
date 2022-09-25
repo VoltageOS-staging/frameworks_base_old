FabricioSC123/frameworks_base-1

base: Custom(User Selected) UDFPS Icon [1/2]
SDB: forward port to A12 & adapt for new implementation of UDFPS Icon

Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsDrawable.java
@@ -19,6 +19,9 @@ package com.android.systemui.biometrics;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.Rect;
@@ -26,7 +29,13 @@ import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.PathShape;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.ParcelFileDescriptor;
+import android.os.RemoteException;
+import android.os.UserHandle;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.util.PathParser;
 
 import androidx.annotation.NonNull;
@@ -37,6 +46,15 @@ import com.android.internal.util.xtended.XtendedUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.tuner.TunerService;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Abstract base class for drawable displayed when the finger is not touching the
@@ -55,6 +73,7 @@ public abstract class UdfpsDrawable extends Drawable {
     private boolean mIlluminationShowing;
 
     int mSelectedIcon = 0;
+    private boolean mCustomFpIconEnabled;
 
     int mAlpha = 255; // 0 - 255
 
@@ -77,7 +96,7 @@ public abstract class UdfpsDrawable extends Drawable {
         init();
     }
 
-    void init() {
+/**    void init() {
         if (XtendedUtils.isPackageInstalled(mContext, udfpsResourcesPackage)) {
             try {
                 PackageManager pm = mContext.getPackageManager();
@@ -99,6 +118,54 @@ public abstract class UdfpsDrawable extends Drawable {
                 }
             };
             Dependency.get(TunerService.class).addTunable(tunable, UDFPS_ICON);
+        }
+    }
+*/
+    void init() {
+        mCustomFpIconEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.OMNI_CUSTOM_FP_ICON_ENABLED, 0) == 1;
+
+        final String customIconURI = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_CUSTOM_FP_ICON,
+                UserHandle.USER_CURRENT);
+
+        if (XtendedUtils.isPackageInstalled(mContext, udfpsResourcesPackage)) {
+            try {
+                PackageManager pm = mContext.getPackageManager();
+                udfpsRes = pm.getResourcesForApplication(udfpsResourcesPackage);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            int res = udfpsRes.getIdentifier("udfps_icons",
+                    "array", udfpsResourcesPackage);
+            mUdfpsIcons = udfpsRes.getStringArray(res);
+
+            TunerService.Tunable tunable = (key, newValue) -> {
+                if (UDFPS_ICON.equals(key)) {
+                    mSelectedIcon = newValue == null ? 0 : Integer.parseInt(newValue);
+                }
+            };
+            Dependency.get(TunerService.class).addTunable(tunable, UDFPS_ICON);
+        }
+        if (!TextUtils.isEmpty(customIconURI) && mCustomFpIconEnabled) {
+            try {
+                ParcelFileDescriptor parcelFileDescriptor =
+                    mContext.getContentResolver().openFileDescriptor(Uri.parse(customIconURI), "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                parcelFileDescriptor.close();
+                mUdfpsDrawable = new BitmapDrawable(mContext.getResources(), image);
+            }
+            catch (Exception e) {
+                mUdfpsDrawable = mSelectedIcon == 0 ? null :
+                        loadDrawable(udfpsRes,
+                                mUdfpsIcons[mSelectedIcon]);
+            }
+        } else {
+            mUdfpsDrawable = mSelectedIcon == 0 ? null :
+                    loadDrawable(udfpsRes,
+                            mUdfpsIcons[mSelectedIcon]);
         }
     }
 

VoltageOS-staging/frameworks_base_old

Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
Change-Id: Ic889f516648a7da9cc4a836aa5c1c49c94617d50

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsDrawable.java
@@ -19,6 +19,9 @@ package com.android.systemui.biometrics;
 import android.content.Context;
 import android.content.res.Resources;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.Rect;
@@ -26,7 +29,13 @@ import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.PathShape;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.ParcelFileDescriptor;
+import android.os.RemoteException;
+import android.os.UserHandle;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.util.PathParser;
 import com.android.internal.util.voltage.VoltageUtils;
 import com.android.systemui.Dependency;
@@ -34,6 +43,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.R;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Abstract base class for drawable displayed when the finger is not touching the
@@ -51,6 +69,7 @@ public abstract class UdfpsDrawable extends Drawable {
     private boolean mIlluminationShowing;
 
     int mSelectedIcon = 0;
+    private boolean mCustomFpIconEnabled;
 
     Drawable mUdfpsDrawable;
     Resources udfpsRes;
@@ -72,7 +91,7 @@ public abstract class UdfpsDrawable extends Drawable {
         init();
     }
 
-    void init() {
+/**    void init() {
         if (VoltageUtils.isPackageInstalled(mContext, udfpsResourcesPackage)) {
             try {
                 PackageManager pm = mContext.getPackageManager();
@@ -94,6 +113,54 @@ public abstract class UdfpsDrawable extends Drawable {
                 }
             };
             Dependency.get(TunerService.class).addTunable(tunable, UDFPS_ICON);
+        }
+    }
+*/
+    void init() {
+        mCustomFpIconEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.OMNI_CUSTOM_FP_ICON_ENABLED, 0) == 1;
+
+        final String customIconURI = Settings.System.getStringForUser(mContext.getContentResolver(),
+                Settings.System.OMNI_CUSTOM_FP_ICON,
+                UserHandle.USER_CURRENT);
+
+        if (VoltageUtils.isPackageInstalled(mContext, udfpsResourcesPackage)) {
+            try {
+                PackageManager pm = mContext.getPackageManager();
+                udfpsRes = pm.getResourcesForApplication(udfpsResourcesPackage);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            int res = udfpsRes.getIdentifier("udfps_icons",
+                    "array", udfpsResourcesPackage);
+            mUdfpsIcons = udfpsRes.getStringArray(res);
+
+            TunerService.Tunable tunable = (key, newValue) -> {
+                if (UDFPS_ICON.equals(key)) {
+                    mSelectedIcon = newValue == null ? 0 : Integer.parseInt(newValue);
+                }
+            };
+            Dependency.get(TunerService.class).addTunable(tunable, UDFPS_ICON);
+        }
+        if (!TextUtils.isEmpty(customIconURI) && mCustomFpIconEnabled) {
+            try {
+                ParcelFileDescriptor parcelFileDescriptor =
+                    mContext.getContentResolver().openFileDescriptor(Uri.parse(customIconURI), "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                parcelFileDescriptor.close();
+                mUdfpsDrawable = new BitmapDrawable(mContext.getResources(), image);
+            }
+            catch (Exception e) {
+                mUdfpsDrawable = mSelectedIcon == 0 ? null :
+                        loadDrawable(udfpsRes,
+                                mUdfpsIcons[mSelectedIcon]);
+            }
+        } else {
+            mUdfpsDrawable = mSelectedIcon == 0 ? null :
+                    loadDrawable(udfpsRes,
+                            mUdfpsIcons[mSelectedIcon]);
         }
     }
 