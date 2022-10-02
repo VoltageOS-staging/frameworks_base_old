CherishOS/android_frameworks_base

Gestures nav: Add Partial Screenshot action.
* Switch to the method in our utils, as this also supports the partial screenshot

Signed-off-by: spezi77 <spezi7713@gmx.net>
Change-Id: Ie8045e35ecb19e4c07113865c6a092859aab9746
Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1024,7 +1024,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 7: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                CherishUtils.takeScreenshot(true);
                 break;
             case 8: // Notification panel
                 ActionUtils.toggleNotifications();
@@ -1052,6 +1052,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 break;
             case 16: // Powermenu
                 CherishUtils.showPowerMenu();
+                break;
+            case 17: // Partial Screenshot
+                CherishUtils.takeScreenshot(false);
                 break;
         }
     }

VoltageOS-staging/frameworks_base_old

Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1031,7 +1031,7 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 ActionUtils.switchScreenOff(mContext);
                 break;
             case 7: // Screenshot
-                ActionUtils.takeScreenshot(true);
+                VoltageUtils.takeScreenshot(true);
                 break;
             case 8: // Notification panel
                 ActionUtils.toggleNotifications();
@@ -1059,6 +1059,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
                 break;
             case 16: // Powermenu
                 VoltageUtils.showPowerMenu();
+                break;
+            case 17: // Partial Screenshot
+                VoltageUtils.takeScreenshot(false);
                 break;
         }
     }