crdroidandroid/android_frameworks_base

Bring back UDFPS icons
neobuddy89: Adapted for Android 12L
Stallix - Adapted for A13 QPR1

Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
Co-authored-by: daniml3 <danimoral1001@gmail.com>
Co-authored-by: DennySPb <dennyspb@gmail.com>
Co-authored-by: kondors1995 <normandija1945@gmail.com>
Co-authored-by: jhenrique09 <jhenrique09.mcz@hotmail.com>
Co-authored-by: Hikari-no-Tenshi <Hikari-no-Tenshi@users.noreply.github.com>
Co-authored-by: TheScarastic <warabhishek@gmail.com>
Co-authored-by: nullxception <nullxception@users.noreply.github.com>
Co-authored-by: Joey Huab <joey@evolution-x.org>
Co-authored-by: 00day0 <00day0@users.noreply.github.com>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsDrawable.java
@@ -1,0 +1,171 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.biometrics;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.pm.PackageManager;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.PathShape;
+import android.provider.Settings;
+import android.util.PathParser;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.android.internal.util.crdroid.Utils;
+import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.tuner.TunerService;
+
+/**
+ * Abstract base class for drawable displayed when the finger is not touching the
+ * sensor area.
+ */
+public abstract class UdfpsDrawable extends Drawable {
+    static final float DEFAULT_STROKE_WIDTH = 3f;
+    static final String UDFPS_ICON = "system:" + Settings.System.UDFPS_ICON;
+
+    String udfpsResourcesPackage = "com.crdroid.udfps.icons";
+
+    @NonNull final Context mContext;
+    @NonNull final ShapeDrawable mFingerprintDrawable;
+    private final Paint mPaint;
+    private boolean mDisplayConfigured;
+
+    int mSelectedIcon = 0;
+
+    Drawable mUdfpsDrawable;
+    Resources udfpsRes;
+    String[] mUdfpsIcons;
+
+    int mAlpha = 255; // 0 - 255
+    public UdfpsDrawable(@NonNull Context context) {
+        mContext = context;
+        final String fpPath = context.getResources().getString(R.string.config_udfpsIcon);
+        mFingerprintDrawable = new ShapeDrawable(
+                new PathShape(PathParser.createPathFromPathData(fpPath), 72, 72));
+        mFingerprintDrawable.mutate();
+
+        mPaint = mFingerprintDrawable.getPaint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        setStrokeWidth(DEFAULT_STROKE_WIDTH);
+
+        init();
+    }
+
+    void init() {
+        if (Utils.isPackageInstalled(mContext, udfpsResourcesPackage)) {
+            try {
+                PackageManager pm = mContext.getPackageManager();
+                udfpsRes = pm.getResourcesForApplication(udfpsResourcesPackage);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            int res = udfpsRes.getIdentifier("udfps_icons",
+                    "array", udfpsResourcesPackage);
+            mUdfpsIcons = udfpsRes.getStringArray(res);
+
+            TunerService.Tunable tunable = (key, newValue) -> {
+                if (UDFPS_ICON.equals(key)) {
+                    mSelectedIcon = newValue == null ? 0 : Integer.parseInt(newValue);
+                    mUdfpsDrawable = mSelectedIcon == 0 ? null :
+                            loadDrawable(udfpsRes,
+                                    mUdfpsIcons[mSelectedIcon]);
+                }
+            };
+            Dependency.get(TunerService.class).addTunable(tunable, UDFPS_ICON);
+        }
+    }
+
+    void setStrokeWidth(float strokeWidth) {
+        mPaint.setStrokeWidth(strokeWidth);
+        invalidateSelf();
+    }
+
+    /**
+     * @param sensorRect the rect coordinates for the sensor area
+     */
+    public void onSensorRectUpdated(@NonNull RectF sensorRect) {
+        final int margin =  (int) sensorRect.height() / 16;
+
+        final Rect bounds = new Rect((int) sensorRect.left + margin,
+                (int) sensorRect.top + margin,
+                (int) sensorRect.right - margin,
+                (int) sensorRect.bottom - margin);
+        updateFingerprintIconBounds(bounds);
+    }
+
+    /**
+     * Bounds for the fingerprint icon
+     */
+    protected void updateFingerprintIconBounds(@NonNull Rect bounds) {
+        mFingerprintDrawable.setBounds(bounds);
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setBounds(bounds);
+        }
+        invalidateSelf();
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        mAlpha = alpha;
+        mFingerprintDrawable.setAlpha(mAlpha);
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setAlpha(mAlpha);
+        }
+        invalidateSelf();
+    }
+
+    boolean isDisplayConfigured() {
+        return mDisplayConfigured;
+    }
+
+    Drawable getUdfpsDrawable() {
+        return mUdfpsDrawable;
+    }
+
+    Drawable loadDrawable(Resources res, String resName) {
+        int resId = res.getIdentifier(resName,
+                "drawable", udfpsResourcesPackage);
+        return res.getDrawable(resId);
+    }
+
+    void setDisplayConfigured(boolean showing) {
+        if (mDisplayConfigured == showing) {
+            return;
+        }
+        mDisplayConfigured = showing;
+        invalidateSelf();
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsKeyguardViewLegacy.java
@@ -29,6 +29,7 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.MathUtils;
 import android.view.View;
@@ -81,6 +82,11 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
     private boolean mFullyInflated;
     private Runnable mOnFinishInflateRunnable;
 
+    private LayoutParams mParams;
+
+    private boolean mCustomUdfpsIcon;
+    private boolean mPackageInstalled;
+
     public UdfpsKeyguardViewLegacy(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mFingerprintDrawable = new UdfpsFpDrawable(context);
@@ -89,6 +95,8 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_x);
         mMaxBurnInOffsetY = context.getResources()
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
+        mPackageInstalled = com.android.internal.util.crdroid.Utils.isPackageInstalled(
+                mContext, "com.crdroid.udfps.icons");
     }
 
     /**
@@ -129,6 +137,14 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
         return true;
     }
 
+    private void updateIcon() {
+        mCustomUdfpsIcon = mPackageInstalled && (Settings.System.getInt(
+                mContext.getContentResolver(), Settings.System.UDFPS_ICON, 0) != 0);
+        mBgProtection.setImageDrawable(mCustomUdfpsIcon
+                ? mFingerprintDrawable :
+                getContext().getDrawable(R.drawable.fingerprint_bg));
+    }
+
     private void updateBurnInOffsets() {
         if (!mFullyInflated) {
             return;
@@ -150,12 +166,14 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
             mLockScreenFp.setTranslationX(mBurnInOffsetX);
             mLockScreenFp.setTranslationY(mBurnInOffsetY);
             mBgProtection.setAlpha(1f - mInterpolatedDarkAmount);
-            mLockScreenFp.setAlpha(1f - mInterpolatedDarkAmount);
+            mLockScreenFp.setAlpha(mCustomUdfpsIcon ? 0.0f
+                : (1f - mInterpolatedDarkAmount));
         } else if (darkAmountForAnimation == 0f) {
             mLockScreenFp.setTranslationX(0);
             mLockScreenFp.setTranslationY(0);
             mBgProtection.setAlpha(mAlpha / 255f);
-            mLockScreenFp.setAlpha(mAlpha / 255f);
+            mLockScreenFp.setAlpha(mCustomUdfpsIcon ? 0.0f
+                : (mAlpha / 255f));
         } else {
             mBgProtection.setAlpha(0f);
             mLockScreenFp.setAlpha(0f);
@@ -313,6 +331,7 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
             updatePadding();
             updateColor();
             updateAlpha();
+            updateIcon();
 
             if (mUseExpandedOverlay) {
                 final LayoutParams lp = (LayoutParams) view.getLayoutParams();

VoltageOS-staging/frameworks_base_old

Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsDrawable.java
@@ -1,0 +1,171 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.biometrics;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.pm.PackageManager;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.PathShape;
+import android.provider.Settings;
+import android.util.PathParser;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.android.internal.util.voltage.VoltageUtils;
+import com.android.systemui.Dependency;
+import com.android.systemui.res.R;
+import com.android.systemui.tuner.TunerService;
+
+/**
+ * Abstract base class for drawable displayed when the finger is not touching the
+ * sensor area.
+ */
+public abstract class UdfpsDrawable extends Drawable {
+    static final float DEFAULT_STROKE_WIDTH = 3f;
+    static final String UDFPS_ICON = "system:" + Settings.System.UDFPS_ICON;
+
+    String udfpsResourcesPackage = "com.power.hub.udfps.icons";
+
+    @NonNull final Context mContext;
+    @NonNull final ShapeDrawable mFingerprintDrawable;
+    private final Paint mPaint;
+    private boolean mDisplayConfigured;
+
+    int mSelectedIcon = 0;
+
+    Drawable mUdfpsDrawable;
+    Resources udfpsRes;
+    String[] mUdfpsIcons;
+
+    int mAlpha = 255; // 0 - 255
+    public UdfpsDrawable(@NonNull Context context) {
+        mContext = context;
+        final String fpPath = context.getResources().getString(R.string.config_udfpsIcon);
+        mFingerprintDrawable = new ShapeDrawable(
+                new PathShape(PathParser.createPathFromPathData(fpPath), 72, 72));
+        mFingerprintDrawable.mutate();
+
+        mPaint = mFingerprintDrawable.getPaint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        setStrokeWidth(DEFAULT_STROKE_WIDTH);
+
+        init();
+    }
+
+    void init() {
+        if (VoltageUtils.isPackageInstalled(mContext, udfpsResourcesPackage)) {
+            try {
+                PackageManager pm = mContext.getPackageManager();
+                udfpsRes = pm.getResourcesForApplication(udfpsResourcesPackage);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            int res = udfpsRes.getIdentifier("udfps_icons",
+                    "array", udfpsResourcesPackage);
+            mUdfpsIcons = udfpsRes.getStringArray(res);
+
+            TunerService.Tunable tunable = (key, newValue) -> {
+                if (UDFPS_ICON.equals(key)) {
+                    mSelectedIcon = newValue == null ? 0 : Integer.parseInt(newValue);
+                    mUdfpsDrawable = mSelectedIcon == 0 ? null :
+                            loadDrawable(udfpsRes,
+                                    mUdfpsIcons[mSelectedIcon]);
+                }
+            };
+            Dependency.get(TunerService.class).addTunable(tunable, UDFPS_ICON);
+        }
+    }
+
+    void setStrokeWidth(float strokeWidth) {
+        mPaint.setStrokeWidth(strokeWidth);
+        invalidateSelf();
+    }
+
+    /**
+     * @param sensorRect the rect coordinates for the sensor area
+     */
+    public void onSensorRectUpdated(@NonNull RectF sensorRect) {
+        final int margin =  (int) sensorRect.height() / 16;
+
+        final Rect bounds = new Rect((int) sensorRect.left + margin,
+                (int) sensorRect.top + margin,
+                (int) sensorRect.right - margin,
+                (int) sensorRect.bottom - margin);
+        updateFingerprintIconBounds(bounds);
+    }
+
+    /**
+     * Bounds for the fingerprint icon
+     */
+    protected void updateFingerprintIconBounds(@NonNull Rect bounds) {
+        mFingerprintDrawable.setBounds(bounds);
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setBounds(bounds);
+        }
+        invalidateSelf();
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        mAlpha = alpha;
+        mFingerprintDrawable.setAlpha(mAlpha);
+        if (mUdfpsDrawable != null) {
+            mUdfpsDrawable.setAlpha(mAlpha);
+        }
+        invalidateSelf();
+    }
+
+    boolean isDisplayConfigured() {
+        return mDisplayConfigured;
+    }
+
+    Drawable getUdfpsDrawable() {
+        return mUdfpsDrawable;
+    }
+
+    Drawable loadDrawable(Resources res, String resName) {
+        int resId = res.getIdentifier(resName,
+                "drawable", udfpsResourcesPackage);
+        return res.getDrawable(resId);
+    }
+
+    void setDisplayConfigured(boolean showing) {
+        if (mDisplayConfigured == showing) {
+            return;
+        }
+        mDisplayConfigured = showing;
+        invalidateSelf();
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}

packages/SystemUI/src/com/android/systemui/biometrics/UdfpsKeyguardViewLegacy.java
@@ -29,6 +29,7 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.MathUtils;
 import android.view.View;
@@ -78,6 +79,11 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
     private boolean mFullyInflated;
     private Runnable mOnFinishInflateRunnable;
 
+    private LayoutParams mParams;
+
+    private boolean mCustomUdfpsIcon;
+    private boolean mPackageInstalled;
+
     public UdfpsKeyguardViewLegacy(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mFingerprintDrawable = new UdfpsFpDrawable(context);
@@ -86,6 +92,8 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_x);
         mMaxBurnInOffsetY = context.getResources()
             .getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
+        mPackageInstalled = com.android.internal.util.voltage.VoltageUtils.isPackageInstalled(
+                mContext, "com.power.hub.udfps.icons");
     }
 
     /**
@@ -126,6 +134,14 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
         return true;
     }
 
+    private void updateIcon() {
+        mCustomUdfpsIcon = mPackageInstalled && (Settings.System.getInt(
+                mContext.getContentResolver(), Settings.System.UDFPS_ICON, 0) != 0);
+        mBgProtection.setImageDrawable(mCustomUdfpsIcon
+                ? mFingerprintDrawable :
+                getContext().getDrawable(R.drawable.fingerprint_bg));
+    }
+
     private void updateBurnInOffsets() {
         if (!mFullyInflated) {
             return;
@@ -148,13 +164,15 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
             mLockScreenFp.setTranslationX(burnInOffsetX);
             mLockScreenFp.setTranslationY(burnInOffsetY);
             mBgProtection.setAlpha(1f - mInterpolatedDarkAmount);
-            mLockScreenFp.setAlpha(1f - mInterpolatedDarkAmount);
+            mLockScreenFp.setAlpha(mCustomUdfpsIcon ? 0.0f
+                : (1f - mInterpolatedDarkAmount));
         } else if (darkAmountForAnimation == 0f) {
             // we're on the lockscreen and should use mAlpha (changes based on shade expansion)
             mLockScreenFp.setTranslationX(0);
             mLockScreenFp.setTranslationY(0);
             mBgProtection.setAlpha(mAlpha / 255f);
-            mLockScreenFp.setAlpha(mAlpha / 255f);
+            mLockScreenFp.setAlpha(mCustomUdfpsIcon ? 0.0f
+                : (mAlpha / 255f));
         } else {
             mBgProtection.setAlpha(0f);
             mLockScreenFp.setAlpha(0f);
@@ -311,6 +329,7 @@ public class UdfpsKeyguardViewLegacy extends UdfpsAnimationView {
                     updatePadding();
                     updateColor();
                     updateAlpha();
+                    updateIcon();
 
                     final LayoutParams lp = (LayoutParams) view.getLayoutParams();
                     lp.width = mSensorBounds.width();