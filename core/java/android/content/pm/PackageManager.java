4b11d37e3f779dc42fafd952f126ed812ef1fa29
LiLei <lei.li.ofcl@gmail.com>
2024-09-07 23:33:45 +0800

    memory leak: Closes leaked instance
    call close() of PackageParser2 to avoid leaking
    
    Change-Id: I0c99d9b445b1520dbd2f035cd2e598e2deba88b0
    Signed-off-by: Lei Li <lei.li.ofcl@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- core/java/android/content/pm/PackageManager.java
+++ core/java/android/content/pm/PackageManager.java
@@ -8822,6 +8822,8 @@ public abstract class PackageManager {
         } catch (PackageParserException e) {
             Log.w(TAG, "Failure to parse package archive apkFile= " +apkFile);
             return null;
+        } finally {
+            parser2.close();
         }
     }
 
